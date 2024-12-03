8bdb815561a4d2a2bbb05f128b4cc22bcb36f315
Neilan Padilla <neilanpadilla5@gmail.com>
2024-12-03 13:22:20 +0800

    removed functions

--- file1.cpp
+++ file1.cpp
@@ -2,190 +2,190 @@
 
 using namespace std;
 
-void lightControl();
-void thermostatSetting();
-void energyUsageCalculator();
-void securitySystemLogin();
-void applianceControlMenu();
-
 int main()
 {
-    int terminator = 0;
-    while (terminator != 6)
+    string userName;
+    cout << "Welcome to Smart Home Automation System" << endl;
+    cout << "Please enter your name: ";
+    cin >> userName;
+    cout << "Hello " << userName << " enjoy your use of Smart Home Automation System" << endl;
+    int userChoice;
+    while (userChoice != 6)
     {
-        int userChoice;
-        cout << "----------------------------\nSmart Home Automation System" << endl
+        cout << "--------------------------------------------------------" << endl
+             << "Current User: " << userName << endl
+             << "Smart Home Automation System" << endl
              << endl
-             << "1. Light Control\n2. Thermostat Settings\n3. Energy Usage Calculator\n4. Security System Login\n5. Appliance Control\n6. Exit\n";
+             << "1. Light Control" << endl
+             << "2. Thermostat Settings" << endl
+             << "3. Energy Usage Calculator" << endl
+             << "4. Security System Login" << endl
+             << "5. Appliance Control" << endl
+             << "6. Exit" << endl;
         cout << endl
              << "Enter Choice: ";
         cin >> userChoice;
-        switch (userChoice)
+        if (userChoice == 1)
         {
-        case 1:
-            lightControl();
-            break;
-        case 2:
-            thermostatSetting();
-            break;
-        case 3:
-            energyUsageCalculator();
-            break;
-        case 4:
-            securitySystemLogin();
-            break;
-        case 5:
-            applianceControlMenu();
-            break;
-        case 6:
-            terminator += 6;
-            cout << "THANK YOU FOR USING SMART HOME AUTOMATION SYSTEM © Neilan Padilla, Shiela Mae De Guzman, Janella Mae Caingcoy, 2024, BSIT-1E";
-            break;
+            cout << "--------------------------------------------------------" << endl
+                 << "LIGHT CONTROL" << endl;
+            int time;
+            cout << "Enter Current Hour in 24-hour format (0 - 23): ";
+            cin >> time;
+            cout << endl;
+            if (time >= 00 && time <= 23)
+            {
+                if (time >= 07 && time <= 17)
+                {
+                    cout << "In the Current Time " << time << ":00" << endl
+                         << "Light Status: Off" << endl;
+                }
+                else if (time <= 6)
+                {
+                    cout << "In the Current Time " << time << ":00" << endl
+                         << "Light Status: On" << endl;
+                }
+                else if (time >= 18)
+                {
+                    cout << "In the Current Time " << time << ":00" << endl
+                         << "Light Status: On" << endl;
+                }
+            }
+            else
+            {
+                cout << "INVALID TIME." << endl;
+            }
         }
-    }
-}
-
-void applianceControlMenu()
-{
-    cout << "----------------------------\nAPPLIANCE CONTROL" << endl;
-    cout << "Please select the letter of your choice. Press (X) to exit.\n"
-         << endl;
-    char menuChoice;
-    do
-    {
-        cout << "A. TV\nB. Air Condition\nY. Refrigerator\nX. Exit\n";
-        cout << "Choice: ";
-        cin >> menuChoice;
-        cout << endl;
-        switch (menuChoice)
+        else if (userChoice == 2)
         {
-        case 'A':
-            cout << "TV turned on" << endl;
-            break;
-        case 'B':
-            cout << "Air Condition turned on" << endl;
-            break;
-        case 'Y':
-            cout << "Refrigerator turned on" << endl;
-            break;
-        case 'X':
-            cout << "EXITING THE MENU... EXIT SUCCESS" << endl;
-            break;
-        default:
-            cout << "Error. (" << menuChoice << ") is not on the available menu choice\n";
-            break;
+            int mode;
+            cout << "--------------------------------------------------------" << endl
+                 << "THERMOSTAT SETTING" << endl;
+            cout << "Please Select Mode. Enter the numeric code for your choice." << endl
+                 << endl
+                 << "1. Cool" << endl
+                 << "2. Heat" << endl
+                 << "3. Fan" << endl
+                 << "4. Off" << endl;
+            cout << "Choice: ";
+            cin >> mode;
+            cout << endl;
+
+            switch (mode)
+            {
+            case 1:
+                cout << "You set the mode to Cool" << endl;
+                cout << "Thermostat Status : COOL" << endl;
+                break;
+            case 2:
+                cout << "You set the mode to Heat" << endl;
+                cout << "Thermostat Status : HEAT" << endl;
+                break;
+            case 3:
+                cout << "You set the mode to Fan" << endl;
+                cout << "Thermostat Status : FAN" << endl;
+                break;
+            case 4:
+                cout << "You set the mode to Off" << endl;
+                cout << "Thermostat Status : OFF" << endl;
+                break;
+            default:
+                cout << "Invalid Mode. Please try again" << endl;
+                break;
+            }
         }
-    } while (menuChoice != 'X');
-}
-
-void securitySystemLogin()
-{
-    cout << "----------------------------\nSECURITY SYSTEM LOGIN" << endl;
-    cout << "Please enter the correct PIN. You only have 3 Attempts.\n"
-         << endl;
-    int correctPin = 12345;
-    int pin;
-    int i = 3;
-    while (pin != correctPin && i > 0)
-    {
-        cout << "Attempt Available: " << i << endl;
-        cout << "Enter Pin: ";
-        cin >> pin;
-        cout << endl;
-        if (pin == correctPin)
+        else if (userChoice == 3)
         {
-            cout << "Access Granted." << endl;
+            int counter;
+            float energyUsage; // we used float if ever user input a value that has decimal
+            float totalUsage = 0;
+            cout << "--------------------------------------------------------" << endl
+                 << "ENERGY USAGE CALCULATOR" << endl;
+            cout << "Weekly Energy Consumptions in kilowatt-hour (kWh)." << endl
+                 << endl;
+            for (counter = 1; counter != 8; counter++)
+            {
+                cout << "Energy Usage Day " << counter << ": ";
+                cin >> energyUsage;
+                totalUsage += energyUsage;
+            }
+            cout << endl
+                 << "Total Energy Usage for the Week: " << totalUsage << " kWh" << endl;
+        }
+        else if (userChoice == 4)
+        {
+            cout << "--------------------------------------------------------" << endl
+                 << "SECURITY SYSTEM LOGIN" << endl;
+            cout << "Please enter the correct PIN. You only have 3 Attempts." << endl
+                 << endl;
+            int correctPin = 12345;
+            int pin;
+            int i = 3;
+            while (pin != correctPin && i > 0)
+            {
+                cout << "Attempt Available: " << i << endl;
+                cout << "Enter Pin: ";
+                cin >> pin;
+                cout << endl;
+                if (pin == correctPin)
+                {
+                    cout << "Access Granted." << endl;
+                }
+                else
+                {
+                    cout << "Incorrect PIN. Try again." << endl;
+                    cout << "--------------------------------------------------------" << endl;
+                    i--;
+                    if (i == 0)
+                    {
+                        cout << "Too many failed attempts. System Locked." << endl;
+                    }
+                }
+            }
+        }
+        else if (userChoice == 5)
+        {
+            cout << "--------------------------------------------------------" << endl
+                 << "APPLIANCE CONTROL" << endl;
+            cout << "Please select the letter of your choice. Press (X) to exit." << endl
+                 << endl;
+            char menuChoice;
+            do
+            {
+                cout << "A. TV" << endl
+                     << "B. Air Condition" << endl
+                     << "Y. Refrigerator" << endl
+                     << "X. Exit" << endl;
+                cout << "Choice: ";
+                cin >> menuChoice;
+                cout << endl;
+                switch (menuChoice)
+                {
+                case 'A':
+                    cout << "TV turned on" << endl;
+                    break;
+                case 'B':
+                    cout << "Air Condition turned on" << endl;
+                    break;
+                case 'Y':
+                    cout << "Refrigerator turned on" << endl;
+                    break;
+                case 'X':
+                    cout << "EXITING THE MENU... EXIT SUCCESS" << endl;
+                    break;
+                default:
+                    cout << "Error. (" << menuChoice << ") is not on the available menu choice. Try again." << endl;
+                    break;
+                }
+            } while (menuChoice != 'X');
+        }
+        else if (userChoice == 6)
+        {
+            cout << "Thank You for using Smart Home Automation System";
         }
         else
         {
-            cout << "Incorrect PIN. Try again." << endl;
-            cout << "----------------------------" << endl;
-            i--;
-            if (i == 0)
-            {
-                cout << "Too many failed attempts. System Locked." << endl;
-            }
+            cout << "Invalid Choice. Try again." << endl;
         }
-    }
-}
-
-void energyUsageCalculator()
-{
-    int i;
-    int counter = 1;
-    float energyUsage; // we used float if ever user input a value that has decimal
-    float totalUsage = 0;
-    cout << "----------------------------\nENERGY USAGE CALCULATOR" << endl;
-    cout << "Weekly Energy Consumptions in kilowatt-hour.\n"
-         << endl;
-    for (i = 0; i != 7; i += 1)
-    {
-        cout << "Energy Usage Day " << counter << ": ";
-        cin >> energyUsage;
-        totalUsage += energyUsage;
-        counter++;
-    }
-    cout << endl
-         << "Total Energy Usage for the Week: " << totalUsage << "kWh" << endl;
-}
-
-void thermostatSetting()
-{
-    int mode;
-    cout << "----------------------------\nTHERMOSTAT SETTING" << endl;
-    cout << "Please Select Mode. Enter the numeric code for your choice.\n\n1. Cool\n2. Heat\n3. Fan\n4. Off\n";
-    cout << "Choice: ";
-    cin >> mode;
-    cout << endl;
-
-    switch (mode)
-    {
-    case 1:
-        cout << "You set the mode to Cool" << endl;
-        cout << "Thermostat Status : COOL" << endl;
-        break;
-    case 2:
-        cout << "You set the mode to Heat" << endl;
-        cout << "Thermostat Status : HEAT" << endl;
-        break;
-    case 3:
-        cout << "You set the mode to Fan" << endl;
-        cout << "Thermostat Status : FAN" << endl;
-        break;
-    case 4:
-        cout << "You set the mode to Off" << endl;
-        cout << "Thermostat Status : OFF" << endl;
-        break;
-    default:
-        cout << "Invalid Mode. Please try again" << endl;
-        break;
-    }
-}
-
-void lightControl()
-{
-    cout << "----------------------------\nLIGHT CONTROL" << endl;
-    int time;
-    cout << "Enter Current Hour in 24-hour format (0 - 23): ";
-    cin >> time;
-    cout << endl;
-    if (time >= 00 && time <= 23)
-    {
-        if (time >= 07 && time <= 17)
-        {
-            cout << "In the Current Time " << time << ":00\nLight Status: Off" << endl;
-        }
-        else if (time <= 6)
-        {
-            cout << "In the Current Time " << time << ":00\nLight Status: On" << endl;
-        }
-        else if (time >= 18)
-        {
-            cout << "In the Current Time " << time << ":00\nLight Status: On" << endl;
-        }
-    }
-    else
-    {
-        cout << "INVALID TIME." << endl;
     }
 }
